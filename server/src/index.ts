5a97b74634c8340d09478f019293c4cd427c46a6
Mohamed Magdy <mohamed_magdy_17593@outlook.com>
2021-01-25 01:17:18 +0200

    init server code with typescript

---
+++ server/src/index.ts
@@ -0,0 +1,10 @@
+import http from 'http';
+import app from './app';
+
+const PORT = process.env.PORT || 4000;
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`listening on *:${PORT}`);
+});
