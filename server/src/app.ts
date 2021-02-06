f730ba4a52c5512c32c1a1ae8a8a469d466801cf
Mohamed Magdy <mohamed_magdy_17593@outlook.com>
2021-02-06 02:25:45 +0200

    add heroku package.json

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,9 @@
+import path from 'path';
 import express from 'express';
 
 const app = express();
+
+// server react app client
+app.use(express.static(path.join(__dirname, '../../client/build')));
 
 export default app;
